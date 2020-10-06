54c679e9c45631e3ed441a0c3bd1692c9729a68c
wrakc <carlosveiga1618@gmail.com>
2020-10-06 13:52:09 -0300

    add friendship create method
    update user.html layout

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,21 @@
 class FriendshipsController < ApplicationController
 
   def create
-    @friendship = current_user.friendships.new(friend_id: params[:user_id], confirmed: false)
+    @friendship = current_user.friendships.build(friend_id: params[:user_id], confirmed: false)
+    
+    if @friendship.save
+      redirect_to users_path, notice: 'Friendship request was sent successfully.'
+    else
+      redirect_to users_path, alert: @friendship.errors.full_messages.join('. ').to_s
+    end
   end
+
+  def accept
+  
+  end
+
+  def reject
+  
+  end
+  
 end
