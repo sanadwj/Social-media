14f403d19c80628bc8798a3d0bd3aaf3e870d1fa
wrakc <carlosveiga1618@gmail.com>
2020-10-07 17:33:01 -0300

    start logic of post of only friends

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,17 @@ class PostsController < ApplicationController
 
   private
 
+  # attr_accessor :a
+
+  # # def get_friends_id
+  # #   current_user.friends.each do |person|
+  # #     @a = person.id
+  # #   end
+  # #   @a  
+  # # end
+
   def timeline_posts
     @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
-
   end
 
   def post_params
