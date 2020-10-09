362db53d7691397106ad043f06d1daad1ec1ec3a
wrakc <carlosveiga1618@gmail.com>
2020-10-09 16:35:12 -0300

    add mutual friendship contorller and view
    update users view layout

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,22 +1,2 @@
 module FriendshipsHelper
-
-  def mutual
-    if show_mutual == nil
-      puts "No Mutual Friend found"
-    else
-      @mutual_friends = User.where(id: show_mutual)
-    end
-  end
-
-  def show_mutual
-    @ids = []
-    current_user.friends.each do |person|
-      person.friends.each do |m|
-        @ids << m.id
-      end
-    end
-    @ids.reject { |x| x == current_user.id }
-
-  end
-
 end
